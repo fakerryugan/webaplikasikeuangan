4cc3fe848fd2e4e31ee2862ce536332aebaac0a9
bk1784 <futonabuu456@gmail.com>
2025-10-23 11:35:41 +0700

    menyelesaikan fitur login, register, dan lupa password. ditambah merekayasa user database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,11 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+           $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('role')->default('user');
+            $table->string('status')->default('1');
             $table->rememberToken();
             $table->timestamps();
         });
